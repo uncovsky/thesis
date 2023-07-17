847a3071bb33e0f6a2166639a4c2b8456fd81e86
uncovsky <vit.uncovsky@gmail.com>
2023-07-17 15:30:52 +0200

    renaming stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,30 @@
 
 int main(){
 
-    Eigen::SparseMatrix<double, Eigen::RowMajor> sex;
-    MDP<int> ahoj;
+    Eigen::SparseMatrix<double, Eigen::RowMajor> mat;
+    MDP<double> ahoj;
 
-    EnvironmentWrapper< size_t, size_t, int, int> env_wrap(&ahoj);
+    EnvironmentWrapper< size_t, size_t, double, int> env_wrap(&ahoj);
 
     auto [s, a, r, succ] = ahoj.reset(10);
 
-    std::cout << s << "\n" << a << "\n" << r << "\n" << succ;
+    //std::cout << s << "\n" << a << "\n" << r << "\n" << succ;
     std::cout << ahoj.name();
 
+    std::cout << strictly_non_dominated<double>({2, 3, 6}, {2, 3, 4}) << std::endl;
+    std::cout << strictly_non_dominated<double>({2, 3, 4}, {2, 3, 4}) << std::endl;
+    std::cout << strictly_non_dominated<double>({2, 3, 2}, {2, 3, 4}) << std::endl;
+
+
+    std::set< std::vector< double > > s1 = { {1, 2, 3}, {2, 3, 4} };
+    std::set< std::vector< double > > s2 = { {1, 2, 2}, {3, 1, 2} };
+
+    nondominated_union(s1, s1);
+
+    for (const auto &x : s1){
+        for (const auto &y : x)
+            std::cout << y;
+        std::cout << "\n";
+    }
+        
 }
